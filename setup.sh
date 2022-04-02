9ceed4eddacf31e9710478610c84550ae8de7084
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-04-02 17:08:04 +0200

    add setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo chmod +x ./ngnx/nginx_config_replace.sh
+sudo chmod +x ./github/pull-api.sh
+sudo chmod +x ./github/pull-ui.sh
+sudo chmod +x ./docker/pull-docker-api.sh
+sudo chmod +x ./docker/pull-docker-ui.sh
